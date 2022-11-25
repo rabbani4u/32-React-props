f8bf32fbcddcfb3bce71b3ee960f6e429e0d1955
rabbani4u <rabbani.iu31@gmail.com>
2022-11-26 00:13:57 +0600

    JSX and ES6

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const name = "Rabbani Khan";
 ReactDOM.render(
 <div>
-<h1>Hello World</h1>
+<h1>Hello {name}</h1>
+<p>Here is your favourite Food</p>
 <ul>
   <li>Coffee</li>
   <li>Coding</li>
