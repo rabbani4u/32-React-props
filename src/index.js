2dac030281a7ecf2a977b602699cc6d49f357a0f
rabbani4u <rabbani.iu31@gmail.com>
2022-11-25 23:58:26 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-<h1>Hello World</h1>,
+<div>
+<h1>Hello World</h1>
+<ul>
+  <li>Coffee</li>
+  <li>Coding</li>
+  <li>Badminton</li>
+</ul>
+</div>,
   document.getElementById('root')
   );
 
